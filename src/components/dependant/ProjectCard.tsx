356e49b6725afea56e69bc7bc08a1f12d75c7d02
zGonzq <gonzaloandres2508@gmail.com>
2024-12-11 16:16:28 -0300

    asd

--- src/components/dependant/ProjectCard.tsx
+++ src/components/dependant/ProjectCard.tsx
@@ -50,12 +50,6 @@ export default function ProjectCard({ title, description, tags, githubUrl, slug,
             <Github className="w-5 h-5" />
             GitHub
           </a>
-          {/* <a
-            href={`/projects/${slug}`}
-            className="px-4 py-2 bg-primary hover:bg-primary-light dark:hover:bg-primary-dark text-white rounded-full transition-color"
-          >
-            Ver más
-          </a> */}
         </div>
       </div>
       </a>
