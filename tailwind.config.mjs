698adc0878e2646d7050187f42bd367f75ee0933
zGonzq <gonzaloandres2508@gmail.com>
2024-12-11 16:23:06 -0300

    onest font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,7 +12,7 @@ export default {
         }
       },
       fontFamily: {
-        sans: ['Inter', 'system-ui', 'sans-serif'],
+        sans: ['Onest', 'system-ui', 'sans-serif'],
       },
       animation: {
         'pulse': 'pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite',
